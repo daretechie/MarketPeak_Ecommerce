3e03ece43bba1c8a18c978d352bbfebf6f76da33
daretechie <deeprince2020@gmail.com>
2025-07-11 09:28:05 +0100

    refactor: Separate JS logic from HTML and improve API URL handling

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 // API base URL
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = `${window.location.protocol}//${window.location.hostname}:8000/api`;
 
 // Token management
 function setToken(token) {
